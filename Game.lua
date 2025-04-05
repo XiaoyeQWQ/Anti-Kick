ddc20f56837478b953d0979f17d7d6c417509bd3
Kaguya <167426134+XiaoyeQWQ@users.noreply.github.com>
2025-04-05 22:22:45 +0800

    Update Game.lua

--- Game.lua
+++ Game.lua
@@ -1,6 +1,6 @@
 local Games = {
-  [116495829188952] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Dead%20Rails.lua", -- Dead Rails
-  [70876832253163] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Dead%20Rails.lua", -- Dead Rails
+  [116495829188952] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/Dead-RailV2.lua", -- Dead Rails
+  [70876832253163] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/Dead-RailV2.lua", -- Dead Rails
   [18687417158] = "https://github.com/XiaoyeQWQ/VEP/raw/refs/heads/main/Forsaken-Vip.txt", -- Forsaken
 }
 return Games
