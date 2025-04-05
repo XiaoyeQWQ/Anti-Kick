1ee502848398b79de4038a0affdf56cb6da09660
Kaguya <167426134+XiaoyeQWQ@users.noreply.github.com>
2025-04-05 22:09:00 +0800

    Create Game.lua

---
+++ Game.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [116495829188952] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Dead%20Rails.lua", -- Dead Rails
+  [70876832253163] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Dead%20Rails.lua", -- Dead Rails
+  [18687417158] = "https://github.com/XiaoyeQWQ/VEP/raw/refs/heads/main/Forsaken-Vip.txt", -- Forsaken
+}
+return Games
